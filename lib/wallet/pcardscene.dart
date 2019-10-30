d1aff7f1d8740aeca7320492d2da906b8cf3b01c
lovecrossyou <lovecrossyoutt@gmail.com>
2019-10-30 16:12:45 +0800

    xx

--- lib/wallet/pcardscene.dart
+++ lib/wallet/pcardscene.dart
@@ -2,6 +2,113 @@ import 'package:flutter/material.dart';
 import 'package:macvon_flutter/common/credit_card.dart';
 import 'package:macvon_flutter/wallet/mock_data.dart';
 
+class HeaderTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            margin: EdgeInsets.only(right: 4),
+            width: 4,
+            height: 13,
+          ),
+          Text('CARD INFORMATION',
+              style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
+
+class CardInfoModel {
+  final String title;
+  final String value;
+
+  CardInfoModel(this.title, this.value);
+}
+
+class CardInfoItem extends StatelessWidget {
+  final CardInfoModel cardInfoModel;
+
+  const CardInfoItem({Key key, @required this.cardInfoModel}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 90,
+            margin: EdgeInsets.only(right: 6),
+            child: Text(this.cardInfoModel.title,
+                style: TextStyle(
+                    color: Colors.blueGrey, fontWeight: FontWeight.bold,fontSize: 12)),
+          ),
+          Text(this.cardInfoModel.value,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14)),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _renderCardInfo() {
+  List<CardInfoModel> list = [
+    new CardInfoModel('Card Number', '.... .... .... 4529'),
+    new CardInfoModel('Card Holder', 'xing zhang'),
+    new CardInfoModel(
+        'Address', '101 Morgon lane Sutie 302 Planinsbort,NJ 08536'),
+    new CardInfoModel('Exp.Date', '··/··'),
+    new CardInfoModel('CVV', '···'),
+  ];
+  return Container(
+    margin: EdgeInsets.fromLTRB(0, 16, 0, 0),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(8),
+    ),
+    child: ListView.separated(
+      shrinkWrap: true,
+      itemCount: list.length,
+      separatorBuilder: (context, index) {
+        return Container(
+          height: .5,
+          margin: EdgeInsets.symmetric(horizontal: 15),
+          color: Color(0xFFDDDDDD),
+        );
+      },
+      itemBuilder: (BuildContext context, int index) {
+        return CardInfoItem(
+          cardInfoModel: list[index],
+        );
+      },
+    ),
+  );
+}
+
+class CardInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      child: Column(
+        children: <Widget>[
+          HeaderTitle(),
+          Container(
+            child: _renderCardInfo(),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 class PhysicalCardScene extends StatelessWidget {
   @override
@@ -9,6 +116,7 @@ class PhysicalCardScene extends StatelessWidget {
     return new ListView(
       children: <Widget>[
         new CreditCard(data: creditCardData),
+        new CardInfo(),
       ],
     );
   }
