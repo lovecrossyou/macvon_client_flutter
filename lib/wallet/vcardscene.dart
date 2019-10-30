2f86d09f52d1ec801c67c36e4224334055ff52f2
lovecrossyou <lovecrossyoutt@gmail.com>
2019-10-30 20:30:10 +0800

    xx

--- lib/wallet/vcardscene.dart
+++ lib/wallet/vcardscene.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:macvon_flutter/common/credit_card.dart';
+import 'package:macvon_flutter/wallet/mock_data.dart';
+
+class VirtualCardList extends StatelessWidget {
+  Widget _renderCard() {
+    return new CreditCard(data: creditCardData);
+  }
+
+  List<Widget> _renderVCardList() {
+    return new List<Widget>.generate(
+        12,
+        (i) => Positioned(
+              top: 120.0 * i ,
+              left: 0.0,
+              right: 0.0,
+              child: _renderCard(),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: _renderVCardList(),
+    );
+  }
+}
 
 class VirtualCardScene extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('VirtualCardScene'),),
-    );
+    return VirtualCardList();
   }
 }
